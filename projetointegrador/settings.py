7cdc031e232ce41ca0e8687444b3da1365075967
Genilson Araújo <48351561+genilsonaraujo@users.noreply.github.com>
2024-10-04 18:51:21 -0300

    Update settings.py

--- projetointegrador/settings.py
+++ projetointegrador/settings.py
@@ -14,8 +14,8 @@ from datetime import timedelta
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'projetointegradors/static')]
 
 # Media files
