145c2fff6f86b1b14596704380c4bb1f6b162dc8
Genilson Araújo <48351561+genilsonaraujo@users.noreply.github.com>
2024-10-28 17:01:15 -0300

    Create 0014_add_preco_unitario_to_itemsaida.py

---
+++ projetointegradors/migrations/0014_add_preco_unitario_to_itemsaida.py
@@ -0,0 +1,14 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('projetointegradors', '0013_previous_migration_name'),  # Atualize para o número correto da última migração
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='itemsaida',
+            name='preco_unitario',
+            field=models.DecimalField(max_digits=10, decimal_places=2, default=0),
+        ),
+    ]
