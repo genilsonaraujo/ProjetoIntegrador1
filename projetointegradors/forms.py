17e5dcd1b69060c422714878bbf679fd693d3a1c
Genilson Araújo <48351561+genilsonaraujo@users.noreply.github.com>
2024-10-28 15:55:13 -0300

    Update forms.py

--- projetointegradors/forms.py
+++ projetointegradors/forms.py
@@ -2,7 +2,7 @@
 from django import forms 
 from .models import Topic, Entry, Produto, Saida, ItemSaida#importacao das clases de models.py
 #Aqui vc cria as classes do formulario
-class TopicForm(forms.ModelForm): #cria classe TopicForm, herda de classe model
+class TopicForm(forms.ModelForm): #cria classe TopicForm, herda de classe de model
     class Meta: #cria classe meta
         model = Topic 
         fields = ['text', 'endereco', 'cnpj', 'telefone']
